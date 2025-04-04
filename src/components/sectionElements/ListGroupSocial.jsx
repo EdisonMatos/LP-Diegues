dfeda5ca2a66a9fa6d0d461465095aff56d09df4
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-04 12:05:47 -0300

    fix: remove comentário de conflito

--- src/components/sectionElements/ListGroupSocial.jsx
+++ src/components/sectionElements/ListGroupSocial.jsx
@@ -4,7 +4,6 @@ import { useState, useEffect } from "react";
 import IconButton from "../interactives/IconButton";
 import Button from "../interactives/Button";
 import { useNavigate } from "react-router-dom";
-// conflito
 
 export default function ListGroupSocial({ LightMode }) {
   const navigate = useNavigate();
