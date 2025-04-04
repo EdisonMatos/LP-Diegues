f51c4b41597908335792971a0d1be4dd75390ee7
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-04-04 12:42:01 -0300

    fix: Ajusta Hero

--- src/components/sectionElements/hero/HeroFundoImage.jsx
+++ src/components/sectionElements/hero/HeroFundoImage.jsx
@@ -118,7 +118,7 @@ export default function HeroFundoImage({ appDownloadButtons }) {
                   )}
                 </div>
               </div>
-              <div className="flex justify-center w-full tablet1:w-[450px] desktop1:w-[42%] desktop2:w-[42.8%]">
+              <div className="flex justify-center w-full phone3:max-w-[423px] tablet1:w-[450px] desktop1:w-[42%] desktop2:w-[42.8%]">
                 <MotionDivDownToUp className="relative flex justify-end w-full ">
                   <div
                     className="bg-center rounded-xl w-full h-[350px] tablet1:h-[380px] desktop1:h-[500px] desktop1:w-[500px] bg-cover bg-no-repeat tablet1:shadow-custom-opacity tablet1:shadow-black/30"
